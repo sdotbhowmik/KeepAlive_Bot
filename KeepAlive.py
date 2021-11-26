c3ded72eccc98c5cf5424c46cc6a35bcb62e5c4a
Subrata K Bhowmik (শুভ) <kingshuvo@gmail.com>
2021-11-26 09:11:15 +0600

    Update KeepAlive.py
    Prtsc removes in this version.

--- KeepAlive.py
+++ KeepAlive.py
@@ -6,11 +6,8 @@ pyautogui.FAILSAFE = False
 print("Now you can minimize this screen and move to other works this will never let your system automatically locked")
 print("By pressing CTRL+C you can terminate this program anytime")
 while True:
-    # time.sleep(240)
-    # for i in range (0,50):
-    #     pyautogui.moveTo(631,i*10)
-    # time.sleep(300)
-    # for i in range (0,1):
-    #     pyautogui.press('prntscrn')
+#  timer for 5 mins.
     time.sleep(300)
-    pyautogui.press('prntscrn')
+    pyautogui.press('ctrl')
+
+
